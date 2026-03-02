30e974be2615b5f521757546572c65ab83f4516f
Jolyne Mangeot <jolyne.mangeot@learner.42.tech>
2026-03-02 21:28:30 +0100

    add Jolyne and Gambini branches

---
+++ pression-sous-pression/Scripts/Game/GameManager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var spawn_interval:float = max(3.0, 10.0 - (current_day * 0.5))
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
