83340935efeb5e383c2f2419c191a6e615f1b420
Jolyne Mangeot <jolyne.mangeot@gmail.com>
2026-03-02 21:28:30 +0100

    add Jolyne and Gambini branches

---
+++ pression-sous-pression/Scripts/Game/DrinkDefinition.gd
@@ -0,0 +1,14 @@
+extends Resource
+
+var drink_name:String
+var icon:Texture2D
+var glass_type:String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
