318fd0a56a0e8e74587c5c479df493bd873f6bde
Jolyne Mangeot <jolyne.mangeot@gmail.com>
2026-03-02 21:47:56 +0100

    add Pacal branch

--- pression-sous-pression/Scripts/Menues/MenuScene.gd
+++ pression-sous-pression/Scripts/Menues/MenuScene.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Exposition Scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Intro Scene.tscn")
 
 
 func _on_settings_button_pressed() -> void:
