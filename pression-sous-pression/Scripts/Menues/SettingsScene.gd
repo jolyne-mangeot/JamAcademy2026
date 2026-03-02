7727dcf73f22dfee57abca78568195476b64764e
Jolyne Mangeot <jolyne.mangeot@gmail.com>
2026-03-02 21:43:35 +0100

    add ben branch

---
+++ pression-sous-pression/Scripts/Menues/SettingsScene.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$CheckButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_back_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu Scene.tscn")
