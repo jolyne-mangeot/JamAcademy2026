62e80bed052deb3e3146f3861e2d011e062b6173
Jolyne Mangeot <jolyne.mangeot@learner.42.tech>
2026-03-02 21:57:33 +0100

    add Caillou branch

---
+++ pression-sous-pression/Scripts/Game/DragAndDrop.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+var is_dragging = false
+var mouse_offset
+var delay = 3
+func _physics_process(delta):
+	if is_dragging == true:
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", get_global_mouse_position(), delay * delta)
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			if get_rect().has_point(to_local(event.position)):
+				is_dragging = true
+		else:
+			is_dragging = false
