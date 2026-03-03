6b9ffb6bafc87a1bfba43d04d8f8513f1fdff661
Camille Panicucci <cpanicuc@c2r10s4.marseille.42.school>
2026-03-03 06:52:49 +0100

    ok

---
+++ pression-sous-pression/Scenes/time_day.gd
@@ -0,0 +1,16 @@
+extends Timer
+@export var hours_per_day: int = 7
+@export var starting_hour: int = 15
+
+@export var seconds_per_hours: int = 30.0
+
+var time_in_hours: float = 0.0
+var total_day_length: float = 0.0
+
+func _ready():
+	total_day_length = hours_per_day
+	time_in_hours = fmod(starting_hour, total_day_length)
+	
+func _process(delta: float) -> void:
+	time_in_hours += delta / seconds_per_hours
+	time_in_hours = fmod(time_in_hours, total_day_length)
