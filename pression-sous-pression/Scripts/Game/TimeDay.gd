3270610d41053f83a70da7b9feac9d40046eccad
Bjmrzd <bjmrzd2611@gmail.com>
2026-03-03 18:18:00 +0100

    push changes

--- pression-sous-pression/Scripts/Game/TimeDay.gd
+++ pression-sous-pression/Scripts/Game/TimeDay.gd
@@ -1,14 +1,17 @@
 extends Timer
-@export var hours_per_day: int = 7
+@export var working_hours_per_day: int = 7
 @export var starting_hour: int = 15
-
 @export var seconds_per_hours: int = 30.0
+
+@onready var timer: Timer = $Level/timer_day
+
+var starting_money: float = 0.0
 
 var time_in_hours: float = 0.0
 var total_day_length: float = 0.0
 
 func _ready():
-	total_day_length = hours_per_day
+	total_day_length = working_hours_per_day
 	time_in_hours = fmod(starting_hour, total_day_length)
 	
 func _process(delta: float) -> void:
