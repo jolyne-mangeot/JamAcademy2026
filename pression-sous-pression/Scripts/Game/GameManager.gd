f0898995228624053eb54027ec5664e703527b4c
Bjmrzd <bjmrzd2611@gmail.com>
2026-03-03 18:18:00 +0100

    push changes

--- pression-sous-pression/Scripts/Game/GameManager.gd
+++ pression-sous-pression/Scripts/Game/GameManager.gd
@@ -1,36 +1,44 @@
 class_name Constants extends RefCounted
 
+var Order_types: Array[String] = ["red", "brown", "blond", "coffee", "pastis"]
+var days: Array[String] = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 
-enum Order {
-	BROWN,
-	BLONDE,
-	RED,
-	COFFEE,
-	PASTIS,
-}
 
-enum Elements {
-	MONDAY,
-	TUESDAY,
-	WEDNESDAY,
-	THURSDAY,
-	FRIDAY,
-	SATURDAY,
-	SUNDAY,
-}
+#enum Order {
+	#BROWN,
+	#BLONDE,
+	#RED,
+	#COFFEE,
+	#PASTIS,
+#}
+
+#enum Elements {
+	#MONDAY,
+	#TUESDAY,
+	#WEDNESDAY,
+	#THURSDAY,
+	#FRIDAY,
+	#SATURDAY,
+	#SUNDAY,
+#}
 
 #day passes
-var current_day = Elements.MONDAY
+var happy_client: bool = false
+var index: int = 0
+var first_day: String = days[0]
+var current_day: String = first_day
 
 func _next_day()-> void:
-	current_day = Elements.values()[(Elements.values().find(current_day) + 1) % Elements.values().size()]
+	current_day = days[index +1]
+	#current_day = Elements.values()[(Elements.values().find(current_day) + 1) % Elements.values().size()]
+	
+	
 
 #cota goes up when day passes
 var current_day_client_quota: int = 10
 
 
 #if clients are happy or not / if they pay
-var happy_client: bool = false
 var satisfaction: int = 0
 var fish_coins: int = 0
 
--- pression-sous-pression/Scripts/Game/GameScene.gd
+++ pression-sous-pression/Scripts/Game/GameScene.gd
@@ -1,5 +1,16 @@
 extends Control
 
+@export var working_hours_per_day: int = 7.0
+@export var starting_hour: float = 15.0
+@export var seconds_per_hours: int = 30.0
+
+@onready var timer: Timer = $Level/timer_day
+
+var starting_money: float = 0.0
+
+var time_in_hours: float = 0.0
+var total_day_length: float = 0.0
+
 @onready var pause_menu: Control = $CanvasLayer/PauseMenu
 
 # Called when the node enters the scene tree for the first time.
@@ -11,7 +22,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().paused = !get_tree().paused
 		pause_menu.visible = get_tree().paused
+
+
+func _on_timer_day_timeout() -> void:
+	pass
