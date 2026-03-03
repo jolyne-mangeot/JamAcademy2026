7d1dc16e8810ec907d48aef329e5405bd43e8d52
Jolyne Mangeot <jolyne.mangeot@gmail.com>
2026-03-03 09:36:01 +0100

    establish signal connections

--- pression-sous-pression/Scripts/Game/Commands.gd
+++ pression-sous-pression/Scripts/Game/Commands.gd
@@ -1,26 +1,26 @@
-extends Node2D
+extends Node
 
 var RNG = RandomNumberGenerator.new()
 
+@export var amount_label:Label
 var num_orders: int = 0
-
 var types: String = ""
+var list_types: Array[String] = ["red", "brown", "blond", "coffee", "pastis"]
+var visual:TextureRect
 
-var list_types: Array[String] = ["red", "brown", "blond", "coffee"]
-
-signal orders_created(types: String, num_orders: int)
 
 func get_random_int(min_val: int, max_val: int) -> int:
 	return RNG.randi_range(min_val, max_val)
 
 func _ready() -> void:
 	RNG.randomize()
-	num_orders = get_random_int(1,9)
+	num_orders = get_random_int(1,4)
 	print(num_orders)
 	types = list_types.pick_random()
+	#visual = get_node(types)
 	print(types)
-	orders_created.emit(types, num_orders)
-	
+	#amount_label.text = "x" + str(num_orders)
+	#visual.visible = true
 
 func _process(delta: float) -> void:
 	pass
