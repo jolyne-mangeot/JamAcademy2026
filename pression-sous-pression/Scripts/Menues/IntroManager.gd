f873582b9748adb539f428f5788a0458b088a323
Jolyne Mangeot <jolyne.mangeot@gmail.com>
2026-03-04 11:35:52 +0100

    intro

---
+++ pression-sous-pression/Scripts/Menues/IntroManager.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@export var slides:Array[Node]
+@export var texts:Array[Node]
+var slide_index: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	slides[slide_index].visible = true
+	texts[slide_index].visible = true
+
+
+func _on_next_pressed() -> void:
+	slide_index += 1
+	if slide_index == 7:
+		get_tree().change_scene_to_file("res://Scenes/Tuto.tscn")
+	else:
+		if slide_index > 0:
+			slides[slide_index - 1].visible = false
+			texts[slide_index - 1].visible = false
+		slides[slide_index].visible = true
+		texts[slide_index].visible = true
