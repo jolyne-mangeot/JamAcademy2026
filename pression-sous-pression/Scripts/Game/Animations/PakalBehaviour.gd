1cb35d1c926b9a0e486abc8ca8eb208f7014acb1
Jolyne Mangeot <jolyne.mangeot@learner.42.tech>
2026-03-04 02:48:38 +0100

    Pakal can now smoke and drink alcohol, affects behavior and dangerousness

--- pression-sous-pression/Scripts/Game/Animations/PakalBehaviour.gd
+++ pression-sous-pression/Scripts/Game/Animations/PakalBehaviour.gd
@@ -1,11 +1,31 @@
 extends Node
 
+@export var side:TextureRect
+@export var front:TextureRect
+@export var sidepipe:TextureRect
+@export var frontpipe:TextureRect
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+var cooldown:float = 0.0
+var pipe:bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if cooldown > 0.0:
+		cooldown -= delta
+		if cooldown <= 0.0:
+			cooldown = 0.0
+			front.visible = false
+			side.visible = true
+			if pipe == true:
+				frontpipe.visible = false
+				sidepipe.visible = true
+
+
+func _on_client_wrong_drink_given() -> void:
+	cooldown = 1.0
+	if sidepipe.visible == true:
+		pipe = true
+		sidepipe.visible = false
+		frontpipe.visible = true
+	side.visible = false
+	front.visible = true
