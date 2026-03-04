9c1c1cfd67c1a9f85754bf0a19ab04855ba6ae79
Bjmrzd <bjmrzd2611@gmail.com>
2026-03-04 11:43:03 +0100

    push back button credits

--- pression-sous-pression/end_credit.gd
+++ pression-sous-pression/end_credit.gd
@@ -14,3 +14,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().paused = !get_tree().paused
 		pause_menu.visible = get_tree().paused
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu Scene.tscn")
