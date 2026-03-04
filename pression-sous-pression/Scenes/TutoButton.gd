76488082f1b2638d45ebf445a0ccc3e05a4c8923
Jolyne <jolyne.mangeot@gmail.com>
2026-03-04 07:54:01 +0100

    Initialisation du Tuto

--- pression-sous-pression/Scenes/TutoButton.gd
+++ pression-sous-pression/Scenes/TutoButton.gd
@@ -3,7 +3,7 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	grab_focus()
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Tuto.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Game Scene.tscn")
