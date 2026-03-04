395178044ecd307c2e7b44e18c31237f945c40a6
Bjmrzd <bjmrzd2611@gmail.com>
2026-03-04 10:36:44 +0100

    phs modif tuto and settings menu

--- pression-sous-pression/Scripts/Game/Animations/PakalWaterControl.gd
+++ pression-sous-pression/Scripts/Game/Animations/PakalWaterControl.gd
@@ -6,12 +6,12 @@ signal drink_given(glass_type: String)
 
 func _ready() -> void:
 	if GameManager.pastis_in_bokal > 0 && GameManager.coffee_in_bokal == 0 && GameManager.beer_in_bokal == 0:
-		get_node(str("Yellow " + str(int(GameManager.pastis_in_bokal / 2)))).visible = true
+		get_node(str("Yellow " + str(int(GameManager.pastis_in_bokal / 2) + 1))).visible = true
 	elif GameManager.beer_in_bokal > 0 && GameManager.coffee_in_bokal == 0 && GameManager.pastis_in_bokal == 0:
-		get_node(str("Brown " + str(int(GameManager.pastis_in_bokal / 2)))).visible = true
+		get_node(str("Brown " + str(int(GameManager.beer_in_bokal / 2) + 1))).visible = true
 	elif GameManager.coffee_in_bokal > 0 || GameManager.beer_in_bokal > 0 || GameManager.pastis_in_bokal > 0:
-		var pipi = GameManager.coffee_in_bokal + GameManager.beer_in_bokal + GameManager.pastis_in_bokal
-		get_node(str("Black " + str(int(pipi / 2)))).visible = true
+		var pipi: int = GameManager.coffee_in_bokal + GameManager.beer_in_bokal + GameManager.pastis_in_bokal
+		get_node(str("Black " + str(int(pipi / 2) + 1))).visible = true
 	else:
 		get_node("Day1 Water").visible = true
 
@@ -29,8 +29,8 @@ func _on_glass_dropped(glass_type: String) -> void:
 		if GameManager.pakal_fed == false:
 			if glass_type == "pipe":
 				pipe.visible = true
-				GameManager.pakal_fed = true
 				GameManager.pakal_smoked_days += 1
+				GameManager.pakal_fed = true
 			if glass_type == "red" || glass_type == "brown"|| glass_type == "blond":
 				GameManager.beer_in_bokal += 1
 				GameManager.pakal_fed = true
